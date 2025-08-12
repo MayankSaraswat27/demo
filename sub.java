d26579cfd69292fdc6d78c670cbfb612cd022fcb
NISHANT SINGH <shubhamnishant04@gmail.com>
2025-08-12 16:18:06 +0530

    Subtraction program

---
+++ sub.java
@@ -0,0 +1,8 @@
+public class sub {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+        int result = a - b;
+        System.out.println("Subtraction result: " + result);
+    }
+}
